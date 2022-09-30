7982b1c07123d276abe10a4415c3496a0aebe356
Mahamat Naim Abdelkerim <73735806+NaimCode@users.noreply.github.com>
2022-09-30 15:40:20 +0100

    added web3

---
+++ web3/contracts/Certification.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+contract Certification {
+    struct Diplome {
+        string nom;
+        string prenom;
+        string intitule;
+        string documentHash;
+        string version;
+        address signataire;
+
+    }
+    function NouveauDiplome(
+        string memory _intitule,
+        string memory _documentHash,
+        string memory _nom,
+        string memory _prenom,
+        string memory _version,
+        address  _signataire
+    ) public {
+        // diplomes_keys.push(_etudiantHash);
+        // Diplomes[_etudiantHash] = Diplome({
+        //     intitule: _intitule,
+        //     etablissement: _etablissement,
+        //     etablissementHash: _etablissementHash,
+        //     documentHash: _documentHash,
+        //     documentLink: _documentLink,
+        //     nom: _nom,
+        //     prenom: _prenom,
+        //     etudiantHash: _etudiantHash
+        // });
+        emit EventCertification(
+            _nom,
+            _prenom,
+            _intitule,
+            _version,
+            _documentHash,
+            _signataire
+        );
+    }
+
+    event EventCertification(
+        string Nom,
+        string Prenom,
+        string Intitule,
+        string Version,
+        string DocumentHash,
+        address Signataire
+    );
+
+    // function ObtenirDiplome(string memory _hash)
+    //     public
+    //     view
+    //     returns (Diplome memory)
+    // {
+    //     return Diplomes[_hash];
+    // }
+
+    // function NombreTotalDeDiplome() public view returns (uint256) {
+    //     return diplomes_keys.length;
+    // }
+
+    // function NombreDeDiplomeParEtablissement(string memory _etablissementHash)
+    //     public
+    //     view
+    //     returns (uint256)
+    // {
+    //     uint256 nombre = 0;
+    //     for (uint256 i = 0; i < diplomes_keys.length; i++) {
+    //         if (
+    //             keccak256(
+    //                 bytes(Diplomes[diplomes_keys[i]].etablissementHash)
+    //             ) == keccak256(bytes(_etablissementHash))
+    //         ) {
+    //             nombre++;
+    //         }
+    //     }
+
+    //     return nombre;
+    // }
+}
