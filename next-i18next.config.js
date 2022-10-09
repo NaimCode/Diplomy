c557cc149264483485dc00a9fc92a308713f357f
Mahamat Naim Abdelkerim <73735806+NaimCode@users.noreply.github.com>
2022-10-09 16:54:27 +0100

    next config changed from module to es

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,9 +1,6 @@
-const path = require('path')
-module.exports = {
-
- i18n: {
-    defaultLocale: "fr",
-    locales: ["en", "fr", "ar"],
-    localePath: path.resolve('./public/locales')
-  },
+import { resolve } from 'path';
+export const i18n = {
+  defaultLocale: "fr",
+  locales: ["en", "fr", "ar"],
+  localePath: resolve('./public/locales')
 };
